af405f9d43882f324b15931b30c9abd4c05babff
ShinSemiya <jun_you@live.jp>
2016-07-10 14:48:35 +0900

    redisの設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  config.cache_store = :redis_store, { host: "localhost",
+                                       port: 6379,
+                                       db: 0,
+                                       namespace: "cache",
+                                       expires_in: 1.days }
 end
