2cdd1c8f63be08cd6be184035aefd5d22e5b1254
ShinSemiya <jun_you@live.jp>
2016-07-10 16:28:01 +0900

    showのspecを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,6 @@ class Item < ApplicationRecord
 
   def image_with_cache
     image_file = File.read(image.path)
-    'data:image/png;base64,' + Base64.strict_encode64(image_file)
+    "data:#{image.content_type};base64," + Base64.strict_encode64(image_file)
   end
 end
